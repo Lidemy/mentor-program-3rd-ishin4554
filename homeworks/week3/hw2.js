7a8a1e555e10b097f0f52e948fd212e8dabee79c
Min Wei <winder3689@gmail.com>
2019-04-24 00:26:59 +0800

    Finish w3 hw2

--- homeworks/week3/hw2.js
+++ homeworks/week3/hw2.js
@@ -1,5 +1,13 @@
 function alphaSwap(str) {
-  console.log(str);
+  let newStr = '';
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i] >= 'a' && str[i] <= 'z') {
+      newStr += str[i].toUpperCase();
+    } else {
+      newStr += str[i].toLowerCase();
+    }
+  }
+  return newStr;
+  // console.log(str);
 }
-
 module.exports = alphaSwap;
