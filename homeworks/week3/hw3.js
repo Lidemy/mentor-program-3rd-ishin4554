e56d2636ee03066cc23093352decbca020aeb1f2
Min Wei <winder3689@gmail.com>
2019-04-24 01:03:19 +0800

    Finish w3 hw3

--- homeworks/week3/hw3.js
+++ homeworks/week3/hw3.js
@@ -1,5 +1,11 @@
 function isPrime(n) {
-  console.log(n);
+  for (let i = 2; i < n; i += 1) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return n !== 1;
+  // console.log(n);
 }
 
 module.exports = isPrime;
