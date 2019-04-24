a00b5909a7e82da13fda656d3cc6a55878c9c9bb
Min Wei <winder3689@gmail.com>
2019-04-25 00:04:40 +0800

    Update string condition

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -2,9 +2,8 @@
 function capitalize(str) {
   let newStr = str;
   // str[0] >= 'a' 就可以取到 ASCII Code
-  if (str[0] >= 'a' && str[0] <= 'z') {
-    newStr = str.replace(str[0], str[0].toUpperCase());
-  }
+  // if (str[0] >= 'a' && str[0] <= 'z') 其實無論如何都會變大寫，所以不用多一個判斷
+  newStr = str.replace(str[0], str[0].toUpperCase());
   return newStr;
 }
 
