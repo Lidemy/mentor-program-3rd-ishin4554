303a57ad99dfd3d9f505819a7c68c3363293164d
Min Wei <winder3689@gmail.com>
2019-04-29 10:34:08 +0800

    Fix guess problems

--- homeworks/week5/httpGame/lv2.js
+++ homeworks/week5/httpGame/lv2.js
@@ -1,17 +1,22 @@
 const request = require('request');
 
+// guess number
+function get(num) {
+  request.get(`https://lidemy-http-challenge.herokuapp.com/lv2?token={HellOWOrld}&id=${num}`,
+    (err, res, body) => {
+      if (body !== '好像不是這本書耶...') {
+        console.log(body);
+        console.log(num);
+      }
+    });
+}
+
+
 request.get('https://lidemy-http-challenge.herokuapp.com/lv2?token={HellOWOrld}',
   (err, res, body) => {
     console.log(body);
   });
 
-request.get('https://lidemy-http-challenge.herokuapp.com/api/books',
-  (err, res, body) => {
-    const obj = JSON.parse(body);
-    console.log(obj);
-  });
-
-request.get('https://lidemy-http-challenge.herokuapp.com/lv2?token={HellOWOrld}&id=56',
-  (err, res, body) => {
-    console.log(body);
-  });
+for (let i = 54; i <= 58; i += 1) {
+  get(i);
+}
