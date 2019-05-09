b02131db5ba806b0cc21e97a2381300434a77da1
Min Wei <winder3689@gmail.com>
2019-05-10 07:26:35 +0800

    Finish w8 hw3

--- homeworks/week8/hw3/js/main.js
+++ homeworks/week8/hw3/js/main.js
@@ -1,9 +1,10 @@
 const request = new XMLHttpRequest();
-function createListItem(cover, avatar, title, name) {
+let offset = 0;
+function createListItem(cover, avatar, title, name, url) {
   const item = document.createElement('li');
   item.innerHTML = `
     <div class="top">
-      <img src="${cover}" />
+    <a href="${url}" target="_blank"><img src="${cover}" /></a>
     </div>
     <div class="bot">
       <img src="${avatar}" />
@@ -18,9 +19,10 @@ function createListItem(cover, avatar, title, name) {
 request.onload = () => {
   if (request.status >= 200 && request.status < 400) {
     const result = JSON.parse(request.responseText);
+    console.log(result);
     result.streams.forEach((stream) => {
       createListItem(stream.preview.medium, stream.channel.logo,
-        stream.channel.status, stream.channel.display_name);
+        stream.channel.status, stream.channel.display_name, stream.channel.url);
     });
   } else {
     console.log('err');
@@ -31,3 +33,11 @@ request.open('GET', `${url}?game=League%20of%20Legends&limit=20`, true);
 request.setRequestHeader('Accept', 'application/vnd.twitchtv.v5+json');
 request.setRequestHeader('Client-ID', 'rck45srex8b3czarkdyskohf45o1fl');
 request.send();
+
+document.querySelector('.btn').onclick = () => {
+  offset += 20;
+  request.open('GET', `${url}?game=League%20of%20Legends&limit=20&offset=${offset}`, true);
+  request.setRequestHeader('Accept', 'application/vnd.twitchtv.v5+json');
+  request.setRequestHeader('Client-ID', 'rck45srex8b3czarkdyskohf45o1fl');
+  request.send();
+};
