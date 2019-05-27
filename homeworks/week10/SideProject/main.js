6dd086f923dadb78bce3ab8e24697e531e7d95d0
Min Wei <winder3689@gmail.com>
2019-05-27 22:14:20 +0800

    Finish side project ver2

--- homeworks/week10/SideProject/main.js
+++ homeworks/week10/SideProject/main.js
@@ -1,41 +1,70 @@
+/* eslint-disable no-undef */
+const col = {
+  id: 0,
+  date: 1,
+  category: 2,
+  description: 3,
+  tag: 4,
+  time: 5,
+};
+
 function dq(selector) {
   return document.querySelector(selector);
 }
 
-class Parser {
+function transDay(day) {
+  return new Date(`${day.slice(0, 4)}-${day.slice(4, 6)}-${day.slice(6, 8)}`);
+}
+
+class Filter {
   constructor(dataset) {
     this.dataset = dataset;
+    this.length = this.getLen();
   }
 
   getByDate(min, max) {
-    const result = this.dataset.filter(d => Number(d[1]) >= min && Number(d[1]) <= max);
-    return new Parser(result);
+    const result = this.dataset.filter(
+      d => Number(d[col.date]) >= min && Number(d[col.date]) <= max,
+    );
+    return new Filter(result);
+  }
+
+  getLen() {
+    const d1 = transDay(this.getCol('date')[0]);
+    const d2 = transDay(this.getCol('date')[this.getCol('date').length - 1]);
+    return (d2 - d1) / 86400000;
   }
 
   getByCategory(cat) {
-    const result = this.dataset.filter(d => d[2] === cat);
-    return new Parser(result);
+    const result = this.dataset.filter(d => d[col.category] === cat);
+    return new Filter(result);
   }
 
   getSum() {
-    let hour = 0;
-    let minute = 0;
-    this.dataset.forEach(d => hour += Number(d[5].split(':')[0]));
-    this.dataset.forEach(d => minute += Number(d[5].split(':')[1]));
+    const timeData = this.getCol('time');
+    const hour = timeData.reduce((acc, d) => acc + Number(d.split(':')[0]), 0);
+    const minute = timeData.reduce((acc, d) => acc + Number(d.split(':')[1]), 0);
     return { hour: hour + Math.floor(minute / 60), minute: minute % 60 };
   }
 
   getAverage(unit = 1) {
     const time = this.getSum();
-    return time.hour / 43 * unit;
+    return time.hour / this.length * unit;
+  }
+
+  getCol(key) {
+    const arr = [];
+    this.dataset.forEach(d => arr.push(d[col[key]]));
+    return arr;
   }
 }
 
-function analysisResult(parser) {
+function analysisResult(filter) {
   const obj = {
-    total: parser.getSum().hour,
-    daily: parser.getAverage(1).toFixed(2),
-    weekly: parser.getAverage(7).toFixed(2),
+    len: filter.length,
+    total: filter.getSum().hour,
+    daily: filter.getAverage(1).toFixed(2),
+    weekly: filter.getAverage(7).toFixed(2),
   };
   return obj;
 }
@@ -44,22 +73,23 @@ function showDashboard(result) {
   const liItem = document.createElement('li');
   liItem.className = 'result__total';
   liItem.innerHTML = `
+    <h1>${result.len}</h1>
     <div>${result.total}</div>
     <div>${result.daily}</div>
     <div>${result.weekly}</div>
   `;
   dq('.dashboard__result').append(liItem);
 }
-function showNumbers(parser) {
+function showNumbers(filter) {
   const min = Number(dq('[name="date_min"]').value);
   const max = Number(dq('[name="date_max"]').value);
   const cat = dq('[name="category"]').value;
   dq('.dashboard__result').innerHTML = '';
-  showDashboard(analysisResult(parser));
-  showDashboard(analysisResult(parser.getByCategory(cat)));
+  showDashboard(analysisResult(filter));
+  showDashboard(analysisResult(filter.getByCategory(cat)));
   if (min && max) {
-    showDashboard(analysisResult(parser.getByDate(min, max)));
-    showDashboard(analysisResult(parser.getByCategory(cat).getByDate(min, max)));
+    showDashboard(analysisResult(filter.getByDate(min, max)));
+    showDashboard(analysisResult(filter.getByCategory(cat).getByDate(min, max)));
   }
 }
 
@@ -70,8 +100,8 @@ function showResult(query) {
   }).then((response) => {
     const data = response.result.values;
     data.shift();
-    const parser = new Parser(data);
-    showNumbers(parser);
+    const filter = new Filter(data);
+    showNumbers(filter);
   });
 }
 
