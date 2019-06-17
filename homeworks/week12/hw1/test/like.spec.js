4aca9d8c07e14d96a5f89d0f9cbde00b3e7a7fcf
Min Wei <winder3689@gmail.com>
2019-06-17 13:48:20 +0800

    Fix bg err

---
+++ homeworks/week12/hw1/test/like.spec.js
@@ -0,0 +1,34 @@
+const superadmin = 'ssss';
+const admin = 'zzzz';
+const normal = 'pkpk';
+
+function login(account) {
+  cy.visit('http://localhost:8080/group1/ishin4554/w12/login.php');
+  cy.get('input[name="username"]')
+    .type(account);
+  cy.get('input[name="password"]')
+    .type(account);
+  cy.get('button[type="submit"]')
+    .click();
+}
+function like() {
+  cy.get('.dash__like').first()
+    .click();
+  cy.get('.nav__logout')
+    .click();
+}
+context('Handle User', () => {
+  it('normal', () => {
+    cy.visit('http://localhost:8080/group1/ishin4554/w12/login.php');
+    login(normal);
+    like();
+  });
+  it('admin', () => {
+    login(admin);
+    like();
+  });
+  it('super admin', () => {
+    login(superadmin);
+    like();
+  });
+});
